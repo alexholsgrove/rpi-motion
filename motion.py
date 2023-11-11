ca8a28a7f017d8a1577ddaa15bbe35bbb73feff1
Alexander Holsgrove <alex.holsgrove@googlemail.com>
2023-11-11 19:58:40 +0000

    Updated to add SCP to remote server

--- motion.py
+++ motion.py
@@ -3,8 +3,14 @@ from picamera2 import Picamera2
 from datetime import datetime
 from time import sleep
 from signal import pause
+import subprocess
 
 # Setup
+server_host = ""
+server_port = "23"
+server_path = ""
+server_user = ""
+
 timestamp_format = "%Y-%m-%d_%H.%M.%S"
 
 print("Setting up PIR...")
@@ -21,19 +27,34 @@ camera.configure(camera_config)
 camera.start()
 print("..done")
 
+
+# Capture a photo
 def take_photo():
     now = datetime.now()
     timestamp = now.strftime(timestamp_format)
-    print("Taking photo", timestamp)
-    camera.capture_file('/home/pi/motion/images/%s.jpg' % timestamp)
+    image_path = f"/home/pi/scripts/rpi-motion/images/{timestamp}.jpg"
+    print("Taking photo", image_path)
+    camera.capture_file(image_path)
     print("Captured")
-    sleep(5)
+    return image_path
+
+
+# Upload image to the server
+def upload_file(file_path):
+    print("Uploading file", file_path)
+    subprocess.run(["scp", "-O", "-P", server_port, file_path, f"{server_user}@{server_host}:{server_path}"])
+
+
+# Callback function for motion detected
+def motion_detected():
+    image_capture = take_photo()
+    upload_file(image_capture)
 
 
 try:
     print("Motion detection ready")
     while True:
-        pir.when_motion = take_photo
+        pir.when_motion = motion_detected
 except KeyboardInterrupt:
     print("Exiting motion")
     pir.when_motion = None
